09eef9c766249cbce8c136a9b52e63703d9c61e2
Fabian <acostahf4@gmail.com>
2022-02-04 11:36:33 -0600

    creating components

---
+++ components/Hero.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "../styles/Hero.module.scss";
+
+const Hero = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div>
+          <h1>Hello, Im Fabian</h1>
+          <p>
+            I love to build experiences that are user-friendly, modern, and just
+            fun!
+          </p>
+          <p>dskfl</p>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
