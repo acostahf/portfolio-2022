58507e33b8d05831ab8753d4cfe01325bee842ac
Fabian <acostahf4@gmail.com>
2022-02-06 13:29:40 -0600

    update text and styles

--- components/About.js
+++ components/About.js
@@ -3,22 +3,17 @@ import styles from "../styles/About.module.scss";
 const About = () => {
   return (
     <div className={styles.container}>
+      <h1>A Few Things About Me</h1>
       <div className={styles.content}>
-        <h1>About ME!</h1>
-        <p>
-          Hello! I&apos;'m Fabian, a software developer based in Austin, TX.
-        </p>
-        <p>
-          I love being part of a team to make a vision into a reality online and
-          have grown to enjoy the front end side of development, whether that is
-          a React project, email templates, or learning a new stack.
-        </p>
-        <p>
-          Since graduating from General Assembly, I had the opportunity to work
-          for Nimaroh studios as a front end developer on a few projects and
-          continued to be a freelance developer for them when they do get a new
-          client looking for a website.
-        </p>
+        <ul>
+          <li>Austin, TX</li>
+          <li>Software Developer for Nimaroh Studios</li>
+          <li>Freelance Developer</li>
+          <li>General Assembly and Austin Community College</li>
+          <li>Car Enthusiast </li>
+          <li>NFT Enthusist</li>
+          <li>Learning blockchain development</li>
+        </ul>
       </div>
     </div>
   );
